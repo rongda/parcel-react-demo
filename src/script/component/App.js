7dd8c197180585417f3223e2ff7779a11ea386d4
Roda <2017@roda.wang>
2018-03-01 14:17:15 +0800

    Add axios

--- src/script/component/App.js
+++ src/script/component/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+	componentWillMount() {
+		axios
+		.get('/api/test')
+		.then((response) => {
+			console.log(response.data);
+		});
+	}
 	render() {
 		return (
-			<div>React</div>
+			<div>Hello React @2018</div>
 		);
 	}
 }
